eee710779263039b6b6b17e21f86444b668335d5
DmytroKutko <32615285+DmytroKutko@users.noreply.github.com>
2018-09-05 08:37:11 +0300

    Add files via upload

---
+++ index.html
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<div align="center">
+    <form name="city">
+        <input name="name" type="text" placeholder="CityName">
+        <input type="button" value="Search" onclick="CitySearch()">
+        <input type="button" value="Current location" onclick="CurrentLoc()">
+    </form>
+
+
+    <table>
+        <tr>
+            <th width="250">Location :</th>
+            <td width="100">
+                <text id="cityName"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>sunrise :</th>
+            <td>
+                <text id="sunrise"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>sunset :</th>
+            <td>
+                <text id="sunset"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>solar_noon :</th>
+            <td>
+                <text id="solar_noon"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>day_length :</th>
+            <td>
+                <text id="day_length"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>civil_twilight_begin :</th>
+            <td>
+                <text id="civil_twilight_begin"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>civil_twilight_end :</th>
+            <td>
+                <text id="civil_twilight_end"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>nautical_twilight_begin :</th>
+            <td>
+                <text id="nautical_twilight_begin"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>nautical_twilight_end :</th>
+            <td>
+                <text id="nautical_twilight_end"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>astronomical_twilight_begin :</th>
+            <td>
+                <text id="astronomical_twilight_begin"></text>
+            </td>
+        </tr>
+        <tr>
+            <th>astronomical_twilight_end :</th>
+            <td>
+                <text id="astronomical_twilight_end"></text>
+            </td>
+        </tr>
+    </table>
+</div>
+</script src="cities.json"></script>
+<script>
+
+</script>
+<script src="script.js"></script>
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,109 @@
+function CitySearch() {
+
+    var cityName = document.city.name.value;
+    cityName = cityName.toLowerCase();
+    cityName = cityName[0].toUpperCase();
+    var cityExist = true;
+
+    $.getJSON("cities.json", function (json) {
+        var city;
+
+        json.forEach(function (element) {
+            if (element.name == cityName) {
+                city = element;
+                cityExist = false;
+                console.log(city);
+            }
+        });
+
+        if (cityExist){
+            alert("This city is not exist")
+        }
+
+        var HttpClient = function () {
+            this.get = function (aUrl, aCallback) {
+                var anHttpRequest = new XMLHttpRequest();
+                anHttpRequest.onreadystatechange = function () {
+                    if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                        aCallback(anHttpRequest.responseText);
+                };
+                anHttpRequest.open("GET", aUrl, true);
+                anHttpRequest.send(null);
+            }
+        };
+
+        var lat = city['lat'];
+        var lng = city['lng'];
+
+        var theurl = 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lng + '&date=today';
+        var client = new HttpClient();
+        client.get(theurl, function (response) {
+            var response1 = JSON.parse(response);
+
+            document.getElementById("cityName").innerHTML = city['name'];
+            document.getElementById("sunrise").innerHTML = response1.results.sunrise;
+            document.getElementById("sunset").innerHTML = response1.results.sunset;
+            document.getElementById("solar_noon").innerHTML = response1.results.solar_noon;
+            document.getElementById("day_length").innerHTML = response1.results.day_length;
+            document.getElementById("civil_twilight_begin").innerHTML = response1.results.civil_twilight_begin;
+            document.getElementById("civil_twilight_end").innerHTML = response1.results.civil_twilight_end;
+            document.getElementById("nautical_twilight_begin").innerHTML = response1.results.nautical_twilight_begin;
+            document.getElementById("nautical_twilight_end").innerHTML = response1.results.nautical_twilight_end;
+            document.getElementById("astronomical_twilight_begin").innerHTML = response1.results.astronomical_twilight_begin;
+            document.getElementById("astronomical_twilight_end").innerHTML = response1.results.astronomical_twilight_end;
+
+        });
+    });
+}
+
+function CurrentLoc() {
+
+    $.getJSON('http://ip-api.com/json', function (data) {
+
+        $.getJSON("cities.json", function (json) {
+
+            var cityName = data['city'];
+            var city;
+
+            json.forEach(function (element) {
+                if (element.name == cityName) {
+                    city = element;
+                }
+            });
+
+            var HttpClient = function () {
+                this.get = function (aUrl, aCallback) {
+                    var anHttpRequest = new XMLHttpRequest();
+                    anHttpRequest.onreadystatechange = function () {
+                        if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                            aCallback(anHttpRequest.responseText);
+                    };
+                    anHttpRequest.open("GET", aUrl, true);
+                    anHttpRequest.send(null);
+                }
+            };
+
+            var lat = city['lat'];
+            var lng = city['lng'];
+
+            var theurl = 'https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lng + '&date=today';
+            var client = new HttpClient();
+            client.get(theurl, function (response) {
+                var response1 = JSON.parse(response);
+
+                document.getElementById("cityName").innerHTML = city['name'];
+                document.getElementById("sunrise").innerHTML = response1.results.sunrise;
+                document.getElementById("sunset").innerHTML = response1.results.sunset;
+                document.getElementById("solar_noon").innerHTML = response1.results.solar_noon;
+                document.getElementById("day_length").innerHTML = response1.results.day_length;
+                document.getElementById("civil_twilight_begin").innerHTML = response1.results.civil_twilight_begin;
+                document.getElementById("civil_twilight_end").innerHTML = response1.results.civil_twilight_end;
+                document.getElementById("nautical_twilight_begin").innerHTML = response1.results.nautical_twilight_begin;
+                document.getElementById("nautical_twilight_end").innerHTML = response1.results.nautical_twilight_end;
+                document.getElementById("astronomical_twilight_begin").innerHTML = response1.results.astronomical_twilight_begin;
+                document.getElementById("astronomical_twilight_end").innerHTML = response1.results.astronomical_twilight_end;
+
+            });
+        });
+    });
+};
